522ae88bbb08230f3f0180c5f1129f493d5f9766
xxxxxxxxxxxxxxxxxxxxxxxxx <xxxxxxxxxxxxxxxxxxxxxxxxx>
2016-10-01 11:58:57 -0400

    page update

--- site/app_modules/nessie.js
+++ site/app_modules/nessie.js
@@ -7,8 +7,21 @@ module.exports = function(app) {
   var module_name = 'nessie';
   var block = {
     app: app,
-    role: 'user'
+    role: 'user',
+    apidata: {
+      key: 'd9258cbfb78477d7caaa4cd76ab13bda',
+      customer_id: '57efd227267ebde464c48989',
+      account_id: '57efd2e5267ebde464c4898a',
+      merchant_id: '57efd667267ebde464c4898d'
+    }
   };
+
+  /*
+  ApiKey: d9258cbfb78477d7caaa4cd76ab13bda
+  customer_id: "57efd227267ebde464c48989"
+  account_id: "57efd2e5267ebde464c4898a"
+  merchant_id: "57efd667267ebde464c4898d"
+  */
 
   block.data = tool.object(require('basedata')(app, module_name));
   block.page = tool.object(require('basepage')(app, module_name, block.data));
@@ -29,9 +42,9 @@ module.exports = function(app) {
   // page
   block.page.index = function(req, res) {
     var page = app.getPage(req, { title:'nessie' });
+    page.apidata = block.apidata;
     res.render('nessie/index', { page:page });
   };
-
 
   // data route
   //app.server.get('/data/nessie/get', block.data.getItemWeb);
